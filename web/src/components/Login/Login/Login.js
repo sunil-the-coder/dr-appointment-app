1f86a84dce31f8a2854cb90b31dc845b414e5312
mangeshshinde <MAng12>
2022-09-27 00:12:10 +0530

    Login and Signup form

--- web/src/components/Login/Login/Login.js
+++ web/src/components/Login/Login/Login.js
@@ -1,67 +1,136 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useContext, useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import React, { useContext } from 'react';
-import { Button } from 'react-bootstrap';
 import { useHistory, useLocation } from 'react-router-dom';
 import { UserContext } from '../../../App';
 import loginBG from '../../../images/loginBG.png';
-//import firebaseConfig from './firebase.config';
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor:'rgba(95,190,154,0.99)',
+        alignItems: 'center',
+        padding: theme.spacing(2),
+        width: '540px',
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '400px',
+        },
+        '& .MuiButtonBase-root': {
+            margin: theme.spacing(2),
+        },
+    },
+}));
 const Login = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     console.log(loggedInUser);
     const history = useHistory();
     const location = useLocation();
     const { from } = location.state || { from: { pathname: '/' } };
+    const classes = useStyles();
+    // create state variables for each input
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLogin, setIsLogin] = useState(true);
 
-    // if (firebase.apps.length === 0) {
-    //     firebase.initializeApp(firebaseConfig);
-    // }
-
-    // const storeAuthToken = () => {
-    //     firebase
-    //         .auth()
-    //         .currentUser.getIdToken(true)
-    //         .then((idToken) => {
-    //             sessionStorage.setItem('token', idToken);
-    //             history.replace(from);
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         });
-    // };
-
-    const handleGoogleSignIn = () => {
-        // const provider = new firebase.auth.GoogleAuthProvider();
-        // firebase
-        //     .auth()
-        //     .signInWithPopup(provider)
-        //     .then((result) => {
-        //         const { displayName, email } = result.user;
-        //         const signedInUser = { name: displayName, email };
-        //         setLoggedInUser(signedInUser);
-        //         storeAuthToken();
-        //     })
-        //     .catch((error) => {
-        //         const errorMessage = error.message;
-        //         console.log(errorMessage);
-        //     });
+    const signUpSubmit = () => {
         setLoggedInUser({ name: "Mangesh Shinde", email:"mangeshshinde@gmail.com"});
-        sessionStorage.setItem('token', "asdffsd");
+        sessionStorage.setItem('token', "SGVsbG8sIHdvcmxkIQ==");
         history.replace(from);
     };
+
+    function loginSubmit() {
+        setLoggedInUser({ name: "Mangesh Shinde", email:email, password: password});
+        sessionStorage.setItem('token', "SGVsbG8sIHdvcmxkIQ==");
+        history.replace(from);
+    }
+
+    function toSignup() {
+        setIsLogin(!isLogin);
+    }
 
     return (
         <div className="login-page container">
             <div className="row align-items-center" style={{ height: '100vh' }}>
-                <div className="col-md-6 shadow p-5">
-                    <div className="from-group text-center">
-                        <Button className="btn btn-brand" onClick={handleGoogleSignIn}>
-                            Sign in
-                        </Button>
+                <div className="col-md-6 shadow p-3">
+                        {!isLogin?
+                            <form className={classes.root} onSubmit={signUpSubmit}>
+                                <TextField
+                                    label="First Name"
+                                    variant="filled"
+                                    required
+                                    value={firstName}
+                                    onChange={e => setFirstName(e.target.value)}
+                                />
+                                <TextField
+                                    label="Last Name"
+                                    variant="filled"
+                                    required
+                                    value={lastName}
+                                    onChange={e => setLastName(e.target.value)}
+                                />
+                                <TextField
+                                    label="Email"
+                                    variant="filled"
+                                    type="email"
+                                    required
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                                <TextField
+                                    label="Password"
+                                    variant="filled"
+                                    type="password"
+                                    required
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                                <div>
+                                    <Button variant="contained" onClick={toSignup}>
+                                        Login
+                                    </Button>
+                                    <Button type="submit" variant="contained" color="primary">
+                                        Signup
+                                    </Button>
+                                </div>
+                            </form>
+                            :
+                            <form className={classes.root} onSubmit={loginSubmit}>
+                                <TextField
+                                    label="Email"
+                                    variant="filled"
+                                    type="email"
+                                    required
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                                <TextField
+                                    label="Password"
+                                    variant="filled"
+                                    type="password"
+                                    required
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                                <div>
+                                    <Button type="submit" variant="contained" color="primary">
+                                        Login
+                                    </Button>
+                                    <Button variant="contained" onClick={toSignup}>
+                                        Signup
+                                    </Button>
+                                </div>
+                            </form>
+                        }
+
                     </div>
-                </div>
                 <div className="col-md-6 d-none d-md-block align-self-end">
                     <img className="img-fluid" src={loginBG} alt="" />
                 </div>
--- web/src/components/Shared/NavBar/NavBar.js
+++ web/src/components/Shared/NavBar/NavBar.js
@@ -5,30 +5,24 @@ import { Link } from 'react-router-dom';
 const NavBar = () => (
     <div>
         <Navbar expand="lg">
-            <Container>
+            <Container className={"nav-bar"}>
                 <Navbar.Brand as={Link} to="/">
                     Doctors Portal
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" >
                     <Nav className="ml-auto">
                         <Nav.Link as={Link} className="mr-4" to="/">
                             Home
                         </Nav.Link>
                         <Nav.Link as={Link} className="mr-4" to="/login">
-                            Login
+                            Log Out
                         </Nav.Link>
                         <Nav.Link as={Link} className="mr-4" to="/dashboard">
                             Dashboard
                         </Nav.Link>
                         <Nav.Link as={Link} className="mr-4 text-white" to="/dashboard">
                             Admin
-                        </Nav.Link>
-                        <Nav.Link as={Link} className="mr-4 text-white" to="#">
-                            Blogs
-                        </Nav.Link>
-                        <Nav.Link as={Link} className="mr-4 text-white" to="#">
-                            Contact Us
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
